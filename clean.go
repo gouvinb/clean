e49f1cb43c0256be6dc68f62314c93fe3b88bbf8
Bruno Gouvinhas <gouvin_b@MacBook-Pro-de-Bruno.local>
2016-01-11 21:50:55 +0100

    Add first strike clean.go.

---
+++ clean.go
@@ -0,0 +1,76 @@
+//
+// file: ./clean.go
+// desc: binary for remove os generated files
+// author:
+// legal:
+//
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+var (
+	all = flag.Bool("all", false, "which include subdirectory")
+)
+
+func main() {
+	flag.Parse()
+
+	if len(os.Args) == 1 {
+		clean("./")
+	} else if os.Args[1] == "-all" ||
+		os.Args[1] == "--all" {
+		clean("./")
+	} else {
+		for _, args := range os.Args[1:] {
+			clean(args)
+		}
+	}
+
+}
+
+func clean(dir string) {
+	files, _ := ioutil.ReadDir(dir)
+	for _, f := range files {
+		if !f.IsDir() && hasPattern(f.Name()) {
+			err := os.Remove(filepath.Clean(dir + "/" + f.Name()))
+			if err != nil {
+				log.Print(err)
+			} else {
+				log.Println("Remove", filepath.Clean(dir+"/"+f.Name()))
+			}
+		} else if !f.IsDir() && !hasPattern(f.Name()) {
+			continue
+		} else {
+			if *all {
+				clean(filepath.Clean(dir + "/" + f.Name()))
+			}
+		}
+	}
+}
+
+func hasPattern(name string) bool {
+	patterns := []string{
+		".DS_Store",
+		".DS_Store?",
+		"._*",
+		".Spotlight-V100",
+		".Trashes",
+		"ehthumbs.db",
+		"Thumbs.db",
+		"~",
+	}
+
+	for _, pattern := range patterns {
+		if strings.Contains(name, pattern) {
+			return true
+		}
+	}
+	return false
+}
